1a7b219f4c5e35293440ebc91111e0696766b05e
scott <scott.m.burdick@gmail.com>
2018-11-13 18:27:09 -0500

    Add missing colon

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
     resource :session, only: [:create, :destroy]
     resource :user, only: [:create]
-  end 
+  end
 
-  root 'static_pages#root'
+  root "static_pages#root"
 end
