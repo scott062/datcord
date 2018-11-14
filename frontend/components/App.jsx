159ea9e67341b426c49d191679046c4adee47415
scott <scott.m.burdick@gmail.com>
2018-11-14 18:21:35 -0500

    Remove modals

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -2,13 +2,11 @@ import React from 'react';
 import LoginFormContainer from './session_form/login_form_container';
 import SignupFormContainer from './session_form/signup_form_container';
 import { Route, Switch } from 'react-router-dom';
-import Modal from './modal';
 import { AuthRoute } from '../util/route_util';
 
 const App = () => {
   return (
     <div>
-      <Modal />
       <header>DATCORD</header>
       <Switch>
         <AuthRoute exact path='/login' component={LoginFormContainer} />
--- frontend/components/session_form/login_form_container.jsx
+++ frontend/components/session_form/login_form_container.jsx
@@ -2,8 +2,6 @@ import { connect } from 'react-redux';
 import React from 'react';
 import { login } from '../../actions/session_actions';
 import sessionForm from './session_form';
-import { openModal, closeModal } from '../../actions/modal_actions';
-
 
 const msp = ({ errors }) => {
   return {
@@ -15,12 +13,6 @@ const msp = ({ errors }) => {
 const mdp = (dispatch) => {
   return {
     formAction: (user) => dispatch(login(user)),
-    otherForm: (
-      <button onClick={() => dispatch(openModal('signup'))}>
-        Signup
-      </button>
-    ),
-    closeModal: () => dispatch(closeModal()),
   };
 };
 
