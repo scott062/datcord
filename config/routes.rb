503c979dca5ef31aefe104426f4b79dc112a7ffb
scott <scott.m.burdick@gmail.com>
2018-11-14 11:15:42 -0500

    Change user routes syntax

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
     resource :session, only: [:create, :destroy]
-    resource :user, only: [:create]
+    resource :users, only: [:create]
   end
 
   root "static_pages#root"
