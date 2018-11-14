88fc580b484ca1f320e34a39b11ef113c70281d3
scott <scott.m.burdick@gmail.com>
2018-11-14 11:29:11 -0500

    Add testing for window

--- frontend/datcord.jsx
+++ frontend/datcord.jsx
@@ -5,3 +5,11 @@ document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
   ReactDOM.render(<h1>Welcome to Datcord</h1>, root);
 });
+
+//testing begins here
+import * as SessionApiUtil from './util/session_api_util';
+
+window.login = SessionApiUtil.login;
+window.signup = SessionApiUtil.signup;
+window.logout = SessionApiUtil.logout;
+//testing ends here
