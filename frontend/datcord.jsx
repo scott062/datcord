4fbc475d58b18539d1d13dd066ee680bee7b9b7d
scott <scott.m.burdick@gmail.com>
2018-11-14 10:33:10 -0500

    Fix entry file

--- frontend/datcord.jsx
+++ frontend/datcord.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 document.addEventListener('DOMContentLoaded', () => {
+  debugger
   const root = document.getElementById('root');
-  ReactDOM.render(<div><h1>Welcome to DatCord</h1></div>, root);
+  ReactDOM.render(<h1>Welcome to DatCord</h1>, root);
 });
