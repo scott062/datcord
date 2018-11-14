4a0e27bc4385b8eee1835cf33fdfc30aeb652893
scott <scott.m.burdick@gmail.com>
2018-11-14 10:33:52 -0500

    Fix spelling

--- frontend/datcord.jsx
+++ frontend/datcord.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 document.addEventListener('DOMContentLoaded', () => {
-  debugger
   const root = document.getElementById('root');
-  ReactDOM.render(<h1>Welcome to DatCord</h1>, root);
+  ReactDOM.render(<h1>Welcome to Datcord</h1>, root);
 });
