07845cdde2793c7fba046ebf818a26680ebe250a
scott <scott.m.burdick@gmail.com>
2018-11-16 15:13:23 -0500

    Seed users db

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,19 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 User.destroy_all
+
 User.create({email: "demo@email.com", password: "password", username: "demoUser"})
+
+User.create({email: "test@email.com", password: "password", username: "test"})
+
+User.create({email: "scott@email.com", password: "password", username: "scott"})
+
+User.create({email: "keith@email.com", password: "password", username: "keith"})
+
+User.create({email: "dustin@email.com", password: "password", username: "dustin"})
+
+User.create({email: "brittany@email.com", password: "password", username: "brittany"})
+
+User.create({email: "regan@email.com", password: "password", username: "regan"})
+
+User.create({email: "david@email.com", password: "password", username: "david"})
