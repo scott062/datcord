da9018d00cf0c13ab4c4d5566265b2f7022ed996
scott <scott.m.burdick@gmail.com>
2018-11-22 14:44:01 -0500

    Delete faulty migration

--- db/migrate/20181117035638_change_servers.rb
+++
@@ -1,9 +0,0 @@
-class ChangeServers < ActiveRecord::Migration[5.2]
-  def change
-    add_column :servers, :admin_id, :integer, null: false
-    add_column :servers, :server_name, :string, null: false
-    add_column :servers, :avatar_url, :string, null: false
-
-    add_timestamps(:servers)
-  end
-end
